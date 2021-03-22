c0fc5ce6347848abbd58fe6ee8899f80e93331c0
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-22 23:46:09 +0700

    feat: add Hit Me button with alert

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
